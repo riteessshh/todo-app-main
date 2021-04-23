8970fdf1d7f24c60d2c1be8c709e41c3dea88b23
Ritesh Jangra <riteshk2874@gmail.com>
2021-04-23 11:19:15 +0530

    starter
    main structure complete and theme changer

---
+++ app.js
@@ -0,0 +1,41 @@
+$(".sun-ico").click(() => {
+  $(".background").toggleClass("background-dark");
+  $(".background").toggleClass("background-light");
+  $(".img-dark").toggleClass("dark-bg");
+  $(".img-dark").toggleClass("light-bg");
+  $(".sun-ico").toggleClass("dark-bg");
+  $(".sun-ico").toggleClass("light-bg");
+  $(".img-light").toggleClass("dark-bg");
+  $(".img-light").toggleClass("light-bg");
+  $(".moon-ico").toggleClass("dark-bg");
+  $(".moon-ico").toggleClass("light-bg");
+  $(".input-section").toggleClass("dark-theme");
+  $(".input-section").toggleClass("light-theme");
+  $(".input-area").toggleClass("dark-theme");
+  $(".input-area").toggleClass("light-theme");
+  $(".list1").toggleClass("dark-theme");
+  $(".list1").toggleClass("light-theme");
+  $(".functions").toggleClass("dark-theme");
+  $(".functions").toggleClass("light-theme");
+})
+
+$(".moon-ico").click(() => {
+  $(".background").toggleClass("background-dark");
+  $(".background").toggleClass("background-light");
+  $(".img-dark").toggleClass("dark-bg");
+  $(".img-dark").toggleClass("light-bg");
+  $(".sun-ico").toggleClass("dark-bg");
+  $(".sun-ico").toggleClass("light-bg");
+  $(".img-light").toggleClass("dark-bg");
+  $(".img-light").toggleClass("light-bg");
+  $(".moon-ico").toggleClass("dark-bg");
+  $(".moon-ico").toggleClass("light-bg");
+  $(".input-section").toggleClass("dark-theme");
+  $(".input-section").toggleClass("light-theme");
+  $(".input-area").toggleClass("dark-theme");
+  $(".input-area").toggleClass("light-theme");
+  $(".list1").toggleClass("dark-theme");
+  $(".list1").toggleClass("light-theme");
+  $(".functions").toggleClass("dark-theme");
+  $(".functions").toggleClass("light-theme");
+})
