2bfc45de9b87ff1bb7a46546722860a4614632e0
Ritesh Jangra <riteshk2874@gmail.com>
2021-04-28 11:47:03 +0530

    list item add or remove function installed

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ $(".sun-ico").click(() => {
   $(".input-section").toggleClass("light-theme");
   $(".input-area").toggleClass("dark-theme");
   $(".input-area").toggleClass("light-theme");
-  $(".list1").toggleClass("dark-theme");
-  $(".list1").toggleClass("light-theme");
+  $("li").toggleClass("dark-theme");
+  $("li").toggleClass("light-theme");
   $(".functions").toggleClass("dark-theme");
   $(".functions").toggleClass("light-theme");
 })
@@ -34,24 +34,63 @@ $(".moon-ico").click(() => {
   $(".input-section").toggleClass("light-theme");
   $(".input-area").toggleClass("dark-theme");
   $(".input-area").toggleClass("light-theme");
-  $(".list1").toggleClass("dark-theme");
-  $(".list1").toggleClass("light-theme");
+  $("li").toggleClass("dark-theme");
+  $("li").toggleClass("light-theme");
   $(".functions").toggleClass("dark-theme");
   $(".functions").toggleClass("light-theme");
 })
 
-list = []
+
 function print() {
   value = document.getElementById("input-data").value;
-  // console.log(value);
-  list.push(value);
-  console.log(list);
+  var node = document.createElement("LI");                 // Create a <li> node
+  var textnode = document.createTextNode(value);         // Create a text node
+  node.appendChild(textnode);                              // Append the text to <li>
+  if (value === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("mylist").appendChild(node);    // append <li> to <ul>
+    $("li").addClass("dark-theme");
+  }
+  document.getElementById("input-data").value = "";
 
-  list.map( (item) => {
-    document.getElementById("output").innerHTML = text;
-  })
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("x");
+  span.className = "close";
+  span.appendChild(txt);
+  node.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
 }
 
-// list.map( (item) => {
-//   document.getElementById("output").innerHTML = item;
-// })
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("IMG");
+  var txt = document.createTextNode("close");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+  $(".close").attr("src", "images/icon-cross.svg");
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
